9f24a034ed72d322c7db53f274a1b60332f1f16b
Mafalda <147611378+hienptx@users.noreply.github.com>
2024-02-09 16:55:58 +0000

    pp

--- bonus_pipex_heredoc.c
+++ bonus_pipex_heredoc.c
@@ -17,14 +17,16 @@
 void heredoc_process(char *arg2)
 {
 	char *str;
+	int		i;
 
+	i = ft_strlen(arg2);
 	while (1)
 	{
 		ft_putstr_fd("pipe heredoc> ", 0);
 		str = get_next_line(0);
 		if (str != NULL)
 		{
-			if (ft_strnstr(str, arg2, ft_strlen(arg2)))
+			if (ft_strncmp(str, arg2, ft_strlen(arg2)) == 0 && str[i] == '\n')
 			{
 				free(str);
 				break;
@@ -38,23 +40,25 @@ void heredoc_process(char *arg2)
 // void	limiter_handling(char *arg2, int *pipefd)
 // {
 // 	char	*str;
+// 	int		i;
 
+// 	i = ft_strlen(arg2);
 // 	while (1)
 // 	{
 // 		ft_putstr_fd("pipe heredoc> ", 0);
 // 		str = get_next_line(0);
 // 		if (str != NULL)
 // 		{
-// 			if (ft_strnstr(str, arg2, ft_strlen(arg2)))
-// 			{
-// 				free(str);
-// 				break ;
+// 			if (ft_strncmp(str, arg2, ft_strlen(arg2)) == 0 && str[i] == '\n')
+// 		{
+//  				free(str);
+// 			break ;
 // 			}
-// 			ft_putstr_fd(str, pipefd[1]);
+//  			ft_putstr_fd(str, pipefd[1]);
 // 			free(str);
 // 		}
+// 		exit(0); 
 // 	}
-// 	exit(0);
 // }
 
 // void	heredoc_process(char *arg2)
@@ -65,16 +69,6 @@ void heredoc_process(char *arg2)
 // 	if (pipe(pipefd) == -1)
 // 		perror_exit("pipe");
 // 	pid = fork();
-// 	if (pid == -1)
-// 		perror_exit("fork");
-// 	if (pid == 0)
-// 	{
-// 		close(pipefd[0]);
-// 		limiter_handling(arg2, pipefd);
-// 		exit(EXIT_SUCCESS);
-// 	}
-// 	else
-// 	{
 // 		close(pipefd[1]);
 // 		dup2(pipefd[0], 0);
 // 		waitpid(pid, NULL, 0);
